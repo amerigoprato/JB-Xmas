8693cbb4391643fe7f72e657063d5a26f5085c45
Amerigo Prato <amerigo.prato@gmail.com>
2014-12-20 21:06:38 +0100

    Update Gruppo.java

--- src/it/unimi/di/Jukebox/Gruppo.java
+++ src/it/unimi/di/Jukebox/Gruppo.java
@@ -1,2 +1,42 @@
-public class Gruppo{
+package it.unimi.di.Jukebox;
+
+/**
+ * Le istanze di questa classe rappresentano gruppi musicali.
+ * @author Amerigo Prato
+ */
+public class Gruppo extends Autore{
+    
+    
+    // ATTRIBUTI //
+    
+    /**
+     * Il nome del Gruppo.
+     */
+    private String nome;
+    
+    
+    // COSTRUTTORI //
+    
+    /**
+     * Costruisce un oggetto che rappresenta un gruppo musicale, dato il nome.
+     * @param n Il nome.
+     */
+    public Gruppo(String n){
+        nome=n;
+    }
+    
+    
+    // METODI //
+    
+    /**
+     * Restituisce il nome del Gruppo.
+     */
+    public String getNome(){
+        return nome;
+    }
+    
+    @Override
+    public String toString(){
+        return nome;
+    }
 }
