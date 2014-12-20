99c6dae39b21ba2776df1437c7cf34c1c1530cfb
Amerigo Prato <amerigo.prato@gmail.com>
2014-12-20 21:08:02 +0100

    Update Persona.java

--- src/it/unimi/di/Jukebox/Persona.java
+++ src/it/unimi/di/Jukebox/Persona.java
@@ -1,2 +1,42 @@
-public class Persona{
+package it.unimi.di.Jukebox;
+
+/**
+ * Le istanze di questa classe rappresentano persone fisiche.
+ * @author Amerigo Prato
+ */
+public class Persona extends Autore{
+    
+    
+    // ATTRIBUTI //
+    
+    /**
+     * Il nome completo della Persona.
+     */
+    private String nome;
+    
+    
+    // COSTRUTTORI //
+    
+    /**
+     * Costruisce un oggetto che rappresenta una persona fisica, dato il nome.
+     * @param n Il nome.
+     */
+    public Persona(String n){
+        nome=n;
+    }
+    
+    
+    // METODI //
+    
+    /**
+     * Restituisce il nome della Persona.
+     */
+    public String getNome(){
+        return nome;
+    }
+    
+    @Override
+    public String toString(){
+        return nome;
+    }
 }
